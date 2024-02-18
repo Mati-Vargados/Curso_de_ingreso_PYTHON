4cc1ed7cb9b6ec390f6f58030a97c08f5304e9ca
Mati-Vargados <mativargados@gmail.com>
2024-02-18 20:19:52 -0300

    mi primer commit

--- 00-Unidades/04_while/Ejercicios_While/While_app_01.py
+++ 00-Unidades/04_while/Ejercicios_While/While_app_01.py
@@ -30,12 +30,44 @@ class App(customtkinter.CTk):
     
     def btn_mostrar_iteracion_on_click(self):
         
-        contador_interacion = 0 
+        numero = prompt("UTN", "Ingrese un numero del 100 al 1000")
 
-        while contador_interacion < 10:
-            contador_interacion += 1 #incremento de la variable de control
-            #contador_interacion = contador_interacion + 1
-            alert("UTN", contador_interacion)
+        numero = int(numero)
+
+        contador_vueltas = 0 
+
+        contador_numeros_pares = 0 
+
+        contador_nultiplo_5 = 0
+
+        suma_multiplos_100 = 0 
+
+        suma_total = 0 
+
+
+        while numero >= 100 and numero <= 1000: 
+
+            if contador_vueltas == numero:
+                break
+
+            contador_vueltas += 1
+            
+            if contador_vueltas % 2 == 0:
+                contador_numeros_pares += 1 
+
+            if contador_vueltas % 5 == 0:
+                contador_nultiplo_5 += 1 
+
+            if contador_vueltas % 100 == 0:
+                suma_multiplos_100 += contador_vueltas
+
+            suma_total += contador_vueltas
+
+            
+
+        alert("UTN", f"Hay {contador_numeros_pares} numeros pares, {contador_nultiplo_5} multiplos de cinco, la suma de los divisibles por cien da {suma_multiplos_100} y la suma total de todos los numeros es de {suma_total}")
+                
+
 
 if __name__ == "__main__":
     app = App()
